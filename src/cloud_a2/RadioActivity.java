59ad37c28b62b630b71083f2021b17c34929dcac
Janice A <janice.po.agustin@gmail.com>
2016-04-10 18:43:24 -0400

    wrapper program to get user input and output what they want

--- src/cloud_a2/RadioActivity.java
+++ src/cloud_a2/RadioActivity.java
@@ -1,5 +1,7 @@
 package cloud_a2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
@@ -12,6 +14,7 @@ import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RunningJob;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.Tool;
@@ -21,14 +24,17 @@ public class RadioActivity extends Configured implements Tool {
 	public static class Map extends Mapper<LongWritable, Text, Text, FloatWritable> {
 //		Log log = LogFactory.getLog(Map.class);
 		String locationProvinceYear;
-		private float totalActivity;
+		private float cs134Activity;
 		
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
+			if (line.contains("Location")) {
+				return;
+			}
 			String[] splitted = line.split(",");
-			locationProvinceYear = splitted[0] + splitted[1] + splitted[5];
-			totalActivity = Float.valueOf(splitted[8]) + Float.valueOf(splitted[11]) + Float.valueOf(splitted[14]) + Float.valueOf(splitted[17]);
-			context.write(new Text(locationProvinceYear), new FloatWritable((int) totalActivity));
+			locationProvinceYear = splitted[0] + "-" + splitted[1] + "-" + splitted[2].substring(0, 3);
+			cs134Activity = Float.valueOf(splitted[11]);	//this is dumb and will not return a lot of results
+			context.write(new Text(locationProvinceYear), new FloatWritable(cs134Activity));
 		}
 	}
 	
@@ -65,11 +71,29 @@ public class RadioActivity extends Configured implements Tool {
 		FileInputFormat.addInputPath(conf, new Path(args[0]));
 		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 		JobClient.runJob(conf);
+		JobClient job = new JobClient(conf);
+		RunningJob runJob = job.submitJob(conf);
+		runJob.waitForCompletion();	//important. Don't return until completed
 		return 0;
 	}
 
 	public static void main(String[] args) throws Exception {
+		System.out.println("Please enter a location, province and year to extract the activity information.");
+		System.out.println("Example: Calgary-AB-2009");
+		String key = System.console().readLine();
+		//run program
 		int res = ToolRunner.run(new Configuration(), new RadioActivity(), args);
+
+		//get output file and extract the line with specified key
+		String outputFile = args[1].endsWith("/") ? args[1] + "part-00000" : args[1] + "/part-00000";
+		try(BufferedReader br = new BufferedReader(new FileReader(outputFile))) {
+			for (String line; (line = br.readLine()) != null;) {
+				if (line.startsWith(key)) {
+					System.out.println(line);
+					break;
+				}
+			}
+		}
 		System.exit(res);
 	}
 }
